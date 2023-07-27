1870644136aec747d4fd8a3f5a6aba6b3c8b0dfd
baihebu <atsushiroadstar@gmail.com>
2023-07-27 16:38:21 +0900

    Update streamlit_app.py
    added breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title('My Parents New Helthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
