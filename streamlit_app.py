2b7c04f08bfbe7c6229958a420aa96d89a96c9a2
baihebu <atsushiroadstar@gmail.com>
2023-07-28 16:56:36 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+streamlit.header("--------")
 
 # pandas.json_normalize()を使うと共通のキーをもつ辞書のリストをpandas.DataFrameに変換できる。
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
