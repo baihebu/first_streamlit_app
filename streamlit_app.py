abcdab6d383689241dca66bcd533d7f155465d46
baihebu <atsushiroadstar@gmail.com>
2023-07-28 16:50:09 +0900

    Update streamlit_app.py
    add comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())  #just writes the data to the screen
 
-# write your own comment -what does the next line do? 
+# pandas.json_normalize()を使うと共通のキーをもつ辞書のリストをpandas.DataFrameに変換できる。
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# streamlit上でデータフレームを表示する
 streamlit.dataframe(fruityvice_normalized)
 
